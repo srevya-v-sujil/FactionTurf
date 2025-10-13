7659aec08484921ab3422ea4e24acd5a117c940e
The-Yearly <theyearlysclub@gmail.com>
2025-10-14 04:49:51 +0530

    Workflow

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,13 @@
 import e, { Router } from "express";
 export const router=Router()
+let mprs=0
 router.get("/getInfo",(req,res)=>{
     res.json({p1:["UP","UP"],p2:["UP","DOWN"],p3:["UP","SHOOT"]})
+})
+
+router.post("/postInfo",(req,res)=>{
+    mprs+=1
+})
+router.get("/getMPRs",(req,res)=>{
+    res.json(mprs)
 })
