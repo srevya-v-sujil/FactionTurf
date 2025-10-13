ccf5335c59796417adfe92cb4f2514a211d2d9cc
The-Yearly <theyearlysclub@gmail.com>
2025-10-14 04:34:31 +0530

    Test Data

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import e, { Router } from "express";
 export const router=Router()
 router.get("/getInfo",(req,res)=>{
-    res.json({moveStack:["UP","UP"]})
+    res.json({p1:["UP","UP"],p2:["UP","DOWN"],p3:["UP","SHOOT"]})
 })
