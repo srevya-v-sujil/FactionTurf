59a7c4f46955c5daf804853fad5a514f8d374f0c
The-Yearly <theyearlysclub@gmail.com>
2025-10-15 01:56:17 +0530

    Tests

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import e, { Router } from "express";
 export const router=Router()
 let mprs=0
+const stack={p1:[],p2:[],p3:[],p4:[]}
 router.get("/getInfo",(req,res)=>{
     res.json({p1:["UP","UP"],p2:["UP","DOWN"],p3:["UP","SHOOT"],p4:["DOWN","SHOOT"]})
 })
 
 router.post("/postInfo",(req,res)=>{
     mprs+=1
+    console.log(req.body)
     res.status(200).json({"message":"Success"})
 })
 router.get("/getMPRs",(req,res)=>{
