6b28949e2c8ac9759ccbe069dd69fd91aa263ae2
The-Yearly <theyearlysclub@gmail.com>
2025-10-15 13:57:37 +0530

    HElp

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -12,7 +12,8 @@ interface stackType{
 let stack:stackType={p1:[],p2:[],p3:[],p4:[]}
 router.get("/getInfo",(req,res)=>{
     const dataStack=stack
-    stack={p1:[],p2:[],p3:[],p4:[]}
+    
+    console.log(dataStack)
     res.status(200).json(dataStack)
 })
 
